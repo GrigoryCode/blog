e15ed45aa5021d8e32005341363ce960529179a4
GrigoryCode <grigorycode@gmail.com>
2019-01-19 16:29:37 +0300

    Version 1.3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,19 @@ class PostsController < ApplicationController
   def new
   end
 
-  def create
-    #render plain: params[:post].inspect
+  def show
+    @post = Post.find(params[:id])
   end
+
+  def create
+
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
