7e783aa82058fd6c5b6ba19e706b84af785fa650
GrigoryCode <grigorycode@gmail.com>
2019-01-19 22:56:53 +0300

    Version 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'update'
+    end
+  end
+
   def create
     @post = Post.new(post_params)
     if(@post.save)
